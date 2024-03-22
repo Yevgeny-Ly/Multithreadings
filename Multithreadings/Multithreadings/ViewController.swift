5427b7af80d3a828aafc2b8959dc6897fa49dfd1
Yevgeny Ly <globchenko16@gmail.com>
2024-03-22 20:41:07 +0300

    - app init

---
+++ Multithreadings/Multithreadings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreadings
+//
+//  Created by Евгений Л on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
